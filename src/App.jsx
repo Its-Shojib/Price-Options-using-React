c017e2d8febe7dc3e55fbdf97551d5d3b9ca82c3
Its_Shojib <mdshojib922@gmail.com>
2023-09-18 08:44:19 +0600

    All Task are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import Navbar from './Components/Navbar/Navbar';
 import Nav from './Components/Nav/Nav';
 import PriceOptions from './Components/PriceOptions/PriceOptions';
+import LineChart from './Components/LineChart/LineChart';
+import Phones from './Components/Phones/Phones';
 function App() {
   return (
     <>
       {/* <Navbar></Navbar> */}
       <Nav></Nav>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
